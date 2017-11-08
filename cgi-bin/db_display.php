f55cdeab961c31d9090a5585ec1a4fb654354de9
Rocket-Hyun <rockethyun@gmail.com>
2017-11-09 03:21:54 +0900

    search finish

--- cgi-bin/db_display.php
+++ cgi-bin/db_display.php
@@ -5,10 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Search Order</title>
+    <style>
+
+    table, th, td {
+        border: 1px solid black
+    }
+    </style>
 </head>
 <body>
 
-<h1>Oder Search Results</h1>
+<h1>Order Search Results</h1>
 <hr />
 <?php
 error_reporting(E_ALL);
@@ -22,7 +28,7 @@ $connect = new mysqli($hostname, $username, $password, $dbname)
      or die("DB Connection Failed");
 
 if($connect) {
- echo("MySQL Server Connect Success!<br />");
+//  echo("MySQL Server Connect Success!<br />");
 }
 else {
  echo("MySQL Server Connect Failed!");
@@ -42,21 +48,19 @@ function test_input($data) {
   return $data;
 }
 
-$sql = "SELECT * FROM PizzaOrders WHERE '$column_name' = '$value'"
-// $result = $connect->query($sql);
-$result=mysql_query($sql);
+$sql = "SELECT * FROM PizzaOrders WHERE ".$column_name." = '$value'";
+$result = $connect->query($sql);
 
-
-echo "<table style='width:100%'> <tr><th>Id Number</th> <th>Name</th><th>Email</th><th>Phone Number</th><th>Topping1</th> <th>Topping2</th><th>Topping3</th><th>Pay Method</th><th>Call First</th></tr><tr>"
+echo "<table style='width:100%'> <tr><th>Id Number</th> <th>Name</th><th>Email</th><th>Phone Number</th><th>Topping1</th> <th>Topping2</th><th>Topping3</th><th>Pay Method</th><th>Call First</th></tr>";
 
 if ($result->num_rows > 0) {
-  while($row=mysql_fetch_array($result)) {
-    echo "<td>".$row["idnumber"]."</td><td>".$row["name"]."</td><td>".$row["email"]."</td><td>".$row["phone_number"]."</td> <td>".$row["topping1"]."</td><td>".$row["topping2"]."</td><td>".$row["topping3"]."</td><td>".$row["pay_method"]."</td><td>".$row["call_first"]."</td>"
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["id_number"]."</td><td>".$row["name"]."</td><td>".$row["email"]."</td><td>".$row["phone_number"]."</td> <td>".$row["topping1"]."</td><td>".$row["topping2"]."</td><td>".$row["topping3"]."</td><td>".$row["pay_method"]."</td><td>".$row["call_first"]."</td></tr>";
   }
 }
 
 
-echo "</tr></table>"
+echo "</table>";
 
 
 $connect->close() ;
